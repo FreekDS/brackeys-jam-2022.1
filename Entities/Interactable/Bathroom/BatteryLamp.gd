ee9051f5e8c50ac199216ee6ea359007d3a00072
Freek <freek.de.sagher21@gmail.com>
2022-02-26 11:28:48 +0100

    LAMP

---
+++ Entities/Interactable/Bathroom/BatteryLamp.gd
@@ -0,0 +1,30 @@
+tool
+extends Interactable
+
+
+onready var modulateColor = Color(5, 5, 1)
+onready var MySprite = $Sprite
+
+var on = false
+
+
+func _ready():
+	enable_on = [
+		StateManager.BATHROOM.INIT
+	]
+	modulate = Color.white
+
+
+func interact():
+	if not can_be_clicked or not enabled:
+		return
+	
+	if on:
+		modulate = Color.white
+		on = false
+	else:
+		modulate = modulateColor
+		on = true
+	
+	complete()
+
--- Entities/Interactable/Garage/Lamp.gd
+++ Entities/Interactable/Garage/Lamp.gd
@@ -5,8 +5,8 @@ func _ready():
 	enable_on = []
 	disable_on = []
 
-func _on_gameState_change(_level, state):
-	if state == StateManager.GARAGE.END:
+func _on_gameState_change(level, state):
+	if state == StateManager.GARAGE.END and level == StateManager.LEVELS.GARAGE:
 		$Sprite.modulate = Color(0.1,0.1,0.1)
 		yield(get_tree().create_timer(3), "timeout")
 		emit_signal("action_end_level")
